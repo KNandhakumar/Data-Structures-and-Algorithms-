09dcd9d17e1300746694d0538ac6dfca6e9d8136
Nandhakumar <nknandhakumar64@gmail.com>
2024-06-20 12:35:24 +0530

    Loops Basics

---
+++ src/NandhuDSA/Loops.java
@@ -0,0 +1,19 @@
+package NandhuDSA;
+
+import java.util.Scanner;
+
+public class Loops {
+    public static void main(String[] args) {
+        /*
+        syntax of for loop :
+        for (initialisation; condition; increment/decrement){
+            // body
+        }
+         */
+
+        // Question : Print numbers from 1 to 5
+        for (int i=1; i<=5; i++){
+            System.out.println(i);
+        }
+    }
+}
