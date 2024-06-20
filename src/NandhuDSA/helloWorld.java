34d99226c12f8f111743039428f0cc460f57b0ac
Nandhakumar <nknandhakumar64@gmail.com>
2024-06-20 12:35:49 +0530

    Loops Basics

--- src/NandhuDSA/helloWorld.java
+++ src/NandhuDSA/helloWorld.java
@@ -1,7 +1,12 @@
 package NandhuDSA;
 
+import java.util.Scanner;
+
 public class helloWorld {
     public static void main(String[] args) {
-        System.out.println("hello World");
+        Scanner input =  new Scanner(System.in);
+        System.out.print("Please input your roll no :");
+        float rollno = input.nextFloat();
+        System.out.println("your roll no 89is : " + rollno);
     }
 }
