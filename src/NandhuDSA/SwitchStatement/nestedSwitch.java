4bfdd6be1fb17083faf227491fe2e239fcfe10e6
Nandhakumar <nknandhakumar64@gmail.com>
2024-06-22 22:08:10 +0530

    Nested Switch Statement

---
+++ src/NandhuDSA/SwitchStatement/nestedSwitch.java
@@ -0,0 +1,35 @@
+package NandhuDSA.SwitchStatement;
+
+import java.util.Scanner;
+
+public class nestedSwitch {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int empId = in.nextInt();
+        String department = in.next();
+        switch (empId) {
+            case 1:
+                System.out.println("Vishnu");
+                break;
+            case 2:
+                System.out.println("gullu");
+                break;
+            case 3:
+                System.out.println("Emp number 3");
+                switch (department) {
+                    case "IT":
+                        System.out.println("IT department");
+                        break;
+                    case "Management":
+                        System.out.println("Management department");
+                        break;
+                    default:
+                        System.out.println("No department entered");
+                }
+                break;
+            default:
+                System.out.println("Enter correct EmpId");
+                break;
+        }
+    }
+}
