372410abade487745bda3051f22976e77f996c14
Nandhakumar <nknandhakumar64@gmail.com>
2024-06-23 17:56:00 +0530

    Array Basic

---
+++ src/NandhuDSA/Arrays/arrayBasic.java
@@ -0,0 +1,16 @@
+package NandhuDSA.Arrays;
+
+public class arrayBasic {
+    public static void main(String[] args) {
+        // Question : store 5 roll numbers
+
+        // Syntax
+        // datatype[] variable_name = new datatype[size];
+
+        int[] rollNo = new int[5];
+        System.out.println(rollNo[0]);
+        int[] arr = {1,2,3,4,5};
+        System.out.println(arr[0]);
+        System.out.println(arr[4]);
+    }
+}
