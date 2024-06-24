33c4a467c5b4eb958f0dc02a8ddcc6534c6bb65a
Nandhakumar <nknandhakumar64@gmail.com>
2024-06-24 11:35:38 +0530

    Question : Search in Strings using linear Search

---
+++ src/NandhuDSA/LinearSearch/SearchInStrings.java
@@ -0,0 +1,36 @@
+package NandhuDSA.LinearSearch;
+
+public class SearchInStrings {
+    public static void main(String[] args) {
+        String name = "Vishnu";
+        char target = 'v';
+        System.out.println(search2(name,target));
+    }
+
+    static boolean search(String str, char target){
+        if (str.length() == 0){
+            return false;
+        }
+
+        for (int i = 0; i <str.length(); i++) {
+            if (target == str.charAt(i)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static boolean search2(String str, char target){
+        if (str.length() == 0){
+            return false;
+        }
+
+        // forEach loop
+        for (char ch : str.toCharArray()){
+            if (target == ch){
+                return true;
+            }
+        }
+        return false;
+    }
+}
