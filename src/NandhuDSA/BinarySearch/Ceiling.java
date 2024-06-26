47ad58dc959a9c14539ff3e3962c4a825f88a008
Nandhakumar <nknandhakumar64@gmail.com>
2024-06-26 23:26:48 +0530

    Leet Code : Search Insert Position Ceiling method

---
+++ src/NandhuDSA/BinarySearch/Ceiling.java
@@ -0,0 +1,28 @@
+package NandhuDSA.BinarySearch;
+
+public class Ceiling {
+    public static void main(String[] args) {
+        // https://leetcode.com/problems/search-insert-position/
+        int[] arr = {2, 3, 5, 9, 14, 16, 18};
+        int target = 15;
+        System.out.println(ceiling(arr,target));
+    }
+
+    static int ceiling(int[] arr, int target){
+        int start = 0;
+        int end = arr.length-1;
+        while (start<=end){
+            int mid = start + (end - start)/2;
+            if (target == arr[mid]){
+                return mid;
+            }
+            if (target>arr[mid]){
+                start = mid+1;
+            }
+            else {
+                end = mid-1;
+            }
+        }
+        return start;
+    }
+}
