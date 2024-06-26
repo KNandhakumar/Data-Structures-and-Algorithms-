13cf5aa77399c01003afd6b9563afbdb96e6d638
Nandhakumar <nknandhakumar64@gmail.com>
2024-06-27 00:23:40 +0530

    Leet Code : Find Smallest Letter Greater Than Target

---
+++ src/NandhuDSA/BinarySearch/findNextGreatestlestter.java
@@ -0,0 +1,25 @@
+package NandhuDSA.BinarySearch;
+
+public class findNextGreatestlestter {
+    // https://leetcode.com/problems/find-smallest-letter-greater-than-target/
+    public static void main(String[] args) {
+        char[] letters = {'c', 'f', 'j'};
+        char target = 'c';
+        System.out.println(nextGreatestLetter(letters,target));
+    }
+
+    static char nextGreatestLetter(char[] letters, char target){
+        int start = 0;
+        int end = letters.length-1;
+        while (start<=end){
+            int mid = start + (end - start)/2;
+            if (target<letters[mid]){
+                end = mid-1;
+            }
+            else{
+                start = mid+1;
+            }
+        }
+        return letters[start % letters.length];
+    }
+}
