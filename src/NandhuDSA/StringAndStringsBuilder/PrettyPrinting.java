ae59db016958bb1cb9200e83b47b1dc56d9824ec
Nandhakumar <nknandhakumar64@gmail.com>
2024-06-30 22:23:05 +0530

    Pretty printing and placeholders lists

---
+++ src/NandhuDSA/StringAndStringsBuilder/PrettyPrinting.java
@@ -0,0 +1,13 @@
+package NandhuDSA.StringAndStringsBuilder;
+
+import java.util.Arrays;
+
+public class PrettyPrinting {
+    public static void main(String[] args) {
+        float a = 453.1244f;
+//        System.out.printf("Formatted number is %.2f", a); // % - placeholder .2 - how many decimal value you want to do f - because it is float
+
+//        System.out.printf("Hello my name is %s and I am %s", "Vishnu", "cool"); // % - placeholder s - String
+        System.out.printf("Hello my name %s and im from %s and my age %s", "vishnu", "tiruvallur" , 19);
+    }
+}
