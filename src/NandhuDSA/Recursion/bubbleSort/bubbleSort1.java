d5cdd4fbb69d737135383e78e4b08d42e15778de
Nandhakumar <nknandhakumar64@gmail.com>
2024-08-03 12:05:46 +0530

    Question : bubble sort in recursion

---
+++ src/NandhuDSA/Recursion/bubbleSort/bubbleSort1.java
@@ -0,0 +1,29 @@
+package NandhuDSA.Recursion.bubbleSort;
+
+import java.util.Arrays;
+
+public class bubbleSort1 {
+    public static void main(String[] args) {
+        int[] arr = {4,3,2,1};
+        bubbleSort(arr,arr.length-1,0);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void bubbleSort(int[] arr, int row,int col){
+        if (row==0){
+            return;
+        }
+        if (row>col){
+            if (arr[col]>arr[col+1]){
+                // swap
+                int temp = arr[col];
+                arr[col] = arr[col+1];
+                arr[col+1] = temp;
+            }
+            bubbleSort(arr,row,col+1);
+        }
+        else {
+            bubbleSort(arr,row-1,0);
+        }
+    }
+}
